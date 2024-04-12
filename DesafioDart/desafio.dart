044f9a67bf4c9496c7e9d8e0f8b11fc8ff591d6a
Arthur Cardoso <105944986+CardosoArthur@users.noreply.github.com>
2024-04-12 15:43:41 -0300

    Adicionando o arquivo base

---
+++ DesafioDart/desafio.dart
@@ -0,0 +1,24 @@
+void main() {
+  //! Detalhe sobre a String
+  //! A String é composta por 4 campos (Nome|Idade|Profissão|Estado onde mora)
+
+  final pacientes = [
+    'Rodrigo Rahman|35|Desenvolvedor|SP',
+    'Manoel Silva|12|Estudante|AL',
+    'Joaquim Rahman|18|Estudante|SP',
+    'Fernando Verne|35|Estudante|PE',
+    'Gustavo Silva|40|Desenvolvedor|CE',
+    'Sandra Silva|40|Desenvolvedor|MG',
+    'Regina Verne|35|Dentista|MG',
+    'João Rahman|55|Jornalista|RJ',
+  ];
+
+  //! Baseado no array acima monte um relatório onde:
+  //! 1 – Inclua nome completo, idade, profissão e estado de origem dos parcipantes da equipe entre os pacientes do array
+  //! 2 – Apresente uma contagem do número total de todos os pacientes do array
+  //! 3 - Apresente os pacientes com mais de 20 anos e print o nome deles
+  //! 4 - Apresente quantos pacientes existem para cada profissão (desenvolvedor, estudante, dentista, jornalista)
+  //! 5 - Apresente a quantidade de pacientes que tenham SP como estado de origem
+  
+  
+}
