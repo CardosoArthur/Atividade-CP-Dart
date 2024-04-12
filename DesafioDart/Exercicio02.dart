b6c72bd2dd548ed0d642ad6b14d271c0958ec519
Arthur Cardoso <105944986+CardosoArthur@users.noreply.github.com>
2024-04-12 19:30:07 -0300

    Adicionando o exercício 02

---
+++ DesafioDart/Exercicio02.dart
@@ -0,0 +1,22 @@
+void main() {
+  //! Detalhe sobre a String
+  //! A String é composta por 4 campos (Nome|Idade|Profissão|Estado onde mora)
+
+  final pacientes = [
+
+    'Rodrigo Rahman|35|Desenvolvedor|SP',
+    'Manoel Silva|12|Estudante|AL',
+    'Joaquim Rahman|18|Estudante|SP',
+    'Fernando Verne|35|Estudante|PE',
+    'Gustavo Silva|40|Desenvolvedor|CE',
+    'Sandra Silva|40|Desenvolvedor|MG',
+    'Regina Verne|35|Dentista|MG',
+    'João Rahman|55|Jornalista|RJ',
+
+  ];
+
+   //! 2 – Apresente uma contagem do número total de todos os pacientes do array
+  
+  print("Total de pacientes: ${pacientes.length}");
+
+}
